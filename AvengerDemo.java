2706aab3319652adf2af792d75dc3debb7fa1ecd
Meetrawal <mnrawal55@gmail.com>
2020-10-14 17:07:55 +0530

    Add files via upload

---
+++ AvengerDemo.java
@@ -0,0 +1,23 @@
+package pakage4;
+
+public class AvengerDemo {
+
+	public static void main(String[] args) {
+		Avenger[] A = new Avenger[2];
+		
+		
+		for (int i =0 ; i<A.length ; i++)
+		{
+			A[i]=new Avenger();
+			System.out.println("Avenger number "+i);
+			A[i].getDetails();
+			System.out.println("\n");
+		}
+		
+		for (int i =0 ; i<A.length ; i++)
+		{
+			A[i].displayDetails();
+		}
+	}
+
+}
